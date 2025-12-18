1e273744d5c921555d7fada364a8f71614e0f26f
aserranoandres <aserranoandres23@gmail.com>
2025-12-18 21:29:49 +0100

    chore: initial project setup

---
+++ src/main/java/com/aserranoandres/inetum_inditex_prices_api/InetumInditexPricesApiApplication.java
@@ -0,0 +1,13 @@
+package com.aserranoandres.inetum_inditex_prices_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InetumInditexPricesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InetumInditexPricesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aserranoandres/inetum_inditex_prices_api/InetumInditexPricesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aserranoandres.inetum_inditex_prices_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InetumInditexPricesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
